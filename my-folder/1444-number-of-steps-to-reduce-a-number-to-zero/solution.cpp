2799916734ff36ecef0aebf077838f50f472d55f
Alberto Vendramini <108136896+avendramini@users.noreply.github.com>
2022-11-04 11:06:17 +0000

    [LeetCode Sync] Runtime - 2 ms (100.00%), Memory - 6 MB (100.00%)

---
+++ my-folder/1444-number-of-steps-to-reduce-a-number-to-zero/solution.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int numberOfSteps(int num) {
+        int n=0;
+        while(num!=0)
+        {
+            n++;
+            if(num%2)
+            {
+                num--;
+            }
+            else
+                num/=2;
+        }
+        return n;
+    }
+};
