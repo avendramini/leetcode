36b546f9537d292ab3c1bb886e9497e0b47fa48a
Alberto Vendramini <108136896+avendramini@users.noreply.github.com>
2024-02-05 14:03:57 +0000

    [LeetCode Sync] Runtime - 6 ms (76.38%), Memory - 12.8 MB (99.00%)

---
+++ my-folder/0300-longest-increasing-subsequence/solution.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int lengthOfLIS(vector<int>& nums) {
+        
+        vector<int> ans;
+        ans.push_back(nums[0]);
+        for(int i=1;i<nums.size();i++)
+        {
+            if(nums[i]>ans[ans.size()-1])
+            {
+                ans.push_back(nums[i]);
+            }
+            else
+            {
+                int it=lower_bound(ans.begin(),ans.end(),nums[i])-ans.begin();
+                ans[it]=nums[i];
+            }
+        }
+        return ans.size();
+    }
+};
