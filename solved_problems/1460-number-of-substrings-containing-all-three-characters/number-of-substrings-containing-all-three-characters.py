5f4e0aeac0753cb3ad1a1f07d39b07f4a97b722a
Alberto Vendramini <108136896+avendramini@users.noreply.github.com>
2025-03-11 12:15:59 +0100

    Time: 66 ms (93.79%) | Memory: 17.9 MB (39.71%) - LeetSync

--- solved_problems/1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.py
+++ solved_problems/1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.py
@@ -1,19 +1,16 @@
 class Solution:
     def numberOfSubstrings(self, s: str) -> int:
-        m={'a':[len(s)]*(len(s)+1), 'b':[len(s)]*(len(s)+1), 'c':[len(s)]*(len(s)+1)}
-        
-        for i,x in enumerate(s[::-1]):
-            m[x][len(s)-i-1]=min([m[x][len(s)-i],len(s)-i-1])
-            for y in ['a','b','c']:
-               m[y][len(s)-i-1]=min([m[y][len(s)-i],m[y][len(s)-i-1]])
-        ris=0
-        print(m)
-        for i,x in enumerate(s):
-            right=i
-            lista=['a','b','c']
-            lista.remove(x)
-            for y in lista:
-                right=max([right,m[y][i]])
-            print(i,right)
-            ris+=len(s)-right
-        return ris
+        # Track last position of a, b, c
+        last_pos = [-1] * 3
+        total = 0
+
+        for pos in range(len(s)):
+            # Update last position of current character
+            last_pos[ord(s[pos]) - ord("a")] = pos
+
+            # Add count of valid substrings ending at current position
+            # If any character is missing, min will be -1
+            # Else min gives leftmost required character position
+            total += 1 + min(last_pos)
+
+        return total
