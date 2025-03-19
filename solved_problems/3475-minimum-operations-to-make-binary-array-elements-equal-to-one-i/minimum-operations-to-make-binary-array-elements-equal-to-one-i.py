79487161cfece33b81f6e4e55d653ac7029b5291
Alberto Vendramini <108136896+avendramini@users.noreply.github.com>
2025-03-19 12:10:18 +0100

    Time: 115 ms (55.27%) | Memory: 21.7 MB (54.50%) - LeetSync

--- solved_problems/3475-minimum-operations-to-make-binary-array-elements-equal-to-one-i/minimum-operations-to-make-binary-array-elements-equal-to-one-i.py
+++ solved_problems/3475-minimum-operations-to-make-binary-array-elements-equal-to-one-i/minimum-operations-to-make-binary-array-elements-equal-to-one-i.py
@@ -1,12 +1,18 @@
+from typing import List
+
 class Solution:
     def minOperations(self, nums: List[int]) -> int:
-        ris=0
-        for i,x in enumerate(nums):
-            if x ==0 and i<=len(nums)-3:
-                nums[i]=1-nums[i]
-                nums[i+1]=1-nums[i+1]
-                nums[i+2]=1-nums[i+2]
-                ris+=1
-            if nums[i]==0:
-                return -1
-        return ris    
+        operations = 0
+        
+        for i in range(len(nums)):
+            if nums[i] == 0:
+                if i > len(nums) - 3:
+                    return -1  # If we can't flip a full triplet, it's impossible
+                
+                # Flip the triplet
+                nums[i] = 1 - nums[i]
+                nums[i+1] = 1 - nums[i+1]
+                nums[i+2] = 1 - nums[i+2]
+                operations += 1
+        
+        return operations
