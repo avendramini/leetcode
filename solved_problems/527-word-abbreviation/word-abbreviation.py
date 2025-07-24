a31a94508343600767e88917f4e637fc7a9e8731
Alberto Vendramini <108136896+avendramini@users.noreply.github.com>
2025-07-24 22:48:09 +0200

    Time: 392 ms (15.22%) | Memory: 25.3 MB (13.04%) - LeetSync

---
+++ solved_problems/527-word-abbreviation/word-abbreviation.py
@@ -0,0 +1,27 @@
+class Solution(object):
+    def wordsAbbreviation(self, words):
+        groups = collections.defaultdict(list)
+        for index, word in enumerate(words):
+            groups[len(word), word[0], word[-1]].append((word, index))
+
+        ans = [None] * len(words)
+        Trie = lambda: collections.defaultdict(Trie)
+        COUNT = False
+        for group in groups.itervalues():
+            trie = Trie()
+            for word, _ in group:
+                cur = trie
+                for letter in word[1:]:
+                    cur[COUNT] = cur.get(COUNT, 0) + 1
+                    cur = cur[letter]
+
+            for word, index in group:
+                cur = trie
+                for i, letter in enumerate(word[1:], 1):
+                    if cur[COUNT] == 1: break
+                    cur = cur[letter]
+                if len(word) - i - 1 > 1:
+                    ans[index] = word[:i] + str(len(word) - i - 1) + word[-1]
+                else:
+                    ans[index] = word
+        return ans
